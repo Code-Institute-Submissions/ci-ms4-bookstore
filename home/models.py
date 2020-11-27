129f80cb6bff4b59d2f7b3e4c84e681907407d98
Patrik <paxicon84@gmail.com>
2020-11-27 08:32:26 +0100

    Added models, admin-registration to Home-app.

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
 # Create your models here.
+
+
+# Add a news model, for blog-posts to display on the main page.
+
+class NewsPost(models.Model):
+
+    title = models.CharField(max_length=255)
+    post = models.CharField(max_length=2500)
+    time = models.DateField(auto_now=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE)
