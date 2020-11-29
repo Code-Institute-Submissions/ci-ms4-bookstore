8fe85862264d6541d7375e77ad175f3eaffbad3c
Patrik <paxicon84@gmail.com>
2020-11-29 14:25:55 +0100

    Add archive view to Home, fixed for-loop for News

--- home/views.py
+++ home/views.py
@@ -5,7 +5,8 @@ from .models import NewsPost
 
 def index(request):
 
-    newsposts = NewsPost.objects.all()
+    # News post sorted by date, newer at the top, limited to five total items
+    newsposts = NewsPost.objects.order_by('-time', 'title')[:5]
 
     context = {
         'news': newsposts
@@ -13,3 +14,14 @@ def index(request):
 
 
     return render(request, 'home/index.html', context)
+
+def archive(request):
+
+    # All items, sorted by date
+    newsposts = NewsPost.objects.order_by('-time')
+
+    context = {
+        'news': newsposts
+    }
+
+    return render(request, 'home/archive.html', context)
