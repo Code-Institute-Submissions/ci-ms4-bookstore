0fff4cf41ac4e18e9e5a585cf359875828440275
Patrik <paxicon84@gmail.com>
2021-02-02 10:36:36 +0100

    Updates to navbar, rest-API functionality

--- searchfilter/urls.py
+++ searchfilter/urls.py
@@ -3,7 +3,7 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'products', views.ProductViewSet, basename='products')
-router.register(r'orders', views.OrderViewSet, basename='orders')
+router.register(r'products', views.ProductViewSet, basename='api_products')
+router.register(r'orders', views.OrderViewSet, basename='api_orders')
 
 urlpatterns = router.urls
